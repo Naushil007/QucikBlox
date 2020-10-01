23040480f743bad50b89a2669d9b1a38d0731c0d
Naushil007 <54878484+Naushil007@users.noreply.github.com>
2020-10-01 12:13:06 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// Application credentials - change to yours (found in QB Dashboard)
+DEFINE('APPLICATION_ID', 'Your Application Key'); // Enter your application ID
+DEFINE('AUTH_KEY', "Your Auth Key"); // Enter yur auth key
+DEFINE('AUTH_SECRET', "Your Auth Secret"); // Enter your secret key
+DEFINE('ACCOUNT_KEY', "Your Account Key"); // Enter your secret key
+// Account Key : RKG1hKxSVd9hxSHJarXm
+// User credentials
+DEFINE('USER_LOGIN', ""); // Your Username
+DEFINE('USER_PASSWORD', ""); // Your Password
+
+// Quickblox endpoints
+DEFINE('QUICKBLOX_API_ENDPOINT', "https://api.quickblox.com/"); // API Endpoints URL 
+DEFINE('QUICKBLOX_PATH_SESSION', "session.json");
+?>
